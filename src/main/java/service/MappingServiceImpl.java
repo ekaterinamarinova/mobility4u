a8286e9f3046906af6c351b129cb0da03475f18f
ekaterinamarinova <katherineeyall@gmail.com>
2020-10-22 23:48:22 +0300

    Fixed parsing logic;

--- src/main/java/service/MappingServiceImpl.java
+++ src/main/java/service/MappingServiceImpl.java
@@ -1,6 +1,5 @@
 package service;
 
-import annotation.TransportationDevice;
 import record.Car;
 import record.CarType;
 import service.definition.MappingService;
@@ -12,6 +11,8 @@ import java.util.List;
 public class MappingServiceImpl implements MappingService{
 
     private static final String COMMA = ",";
+    private static final String WHITE_SPACE = " ";
+    private static final String EMPTY_STR = "";
     private static final String POSTFIX = "_car";
 
     public List<? super Serializable> mapToObject(List<String> lines) {
@@ -19,10 +20,16 @@ public class MappingServiceImpl implements MappingService{
         var fields = new String[]{};
 
         for (String s:lines) {
-            var type = s.toLowerCase().substring(0, s.indexOf(POSTFIX));
+            var type = s.toLowerCase().substring(0, s.toLowerCase().indexOf(WHITE_SPACE));
+
             fields = s.toLowerCase()
-                    .substring(s.indexOf(POSTFIX))
-                    .split(COMMA);
+                    .replaceAll("\s++", EMPTY_STR)
+                    .substring(
+                            s.toLowerCase()
+                                .lastIndexOf(
+                                        POSTFIX
+                                ) + POSTFIX.length()
+                    ).split(COMMA);
 
             mapToSpecificObject(type, objList, fields);
         }
@@ -34,18 +41,17 @@ public class MappingServiceImpl implements MappingService{
                                      List<? super Serializable> objList,
                                      String[] fields) {
         switch (type) {
-            case CarType.GAS: objList.add(new Car(type, fields[1],
+            case CarType.GAS: objList.add(new Car(
+                    type,
+                    fields[1],
                     fields[2],
                     fields[4],
-                    Double.valueOf(fields[5]),
                     fields[3]
             )); break;
             case CarType.ELECTRIC: objList.add(new Car(type, fields[1],
                     fields[2],
                     fields[4],
-                    Double.valueOf(fields[5]),
-                    fields[3],
-                    fields[6]
+                    null
             )); break;
             case CarType.HYBRID: break;
             default:break;
