8ce4eb6b7da3f61575e7e3c178651da48d9c8ace
ekaterinamarinova <katherineeyall@gmail.com>
2020-10-23 00:09:34 +0300

    added equals, hashcode, toString

--- src/main/java/record/Car.java
+++ src/main/java/record/Car.java
@@ -1,9 +1,41 @@
 package record;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Objects;
 
 public record Car(String type, String brand,
                   String model, String power,
                   String price, String... otherProperties) implements Serializable {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(type, car.type) &&
+                Objects.equals(brand, car.brand) &&
+                Objects.equals(model, car.model) &&
+                Objects.equals(power, car.power) &&
+                Objects.equals(price, car.price) &&
+                Arrays.equals(otherProperties, car.otherProperties);
+    }
 
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(type, brand, model, power, price);
+        result = 31 * result + Arrays.hashCode(otherProperties);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "type='" + type + '\'' +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", power='" + power + '\'' +
+                ", price='" + price + '\'' +
+                ", otherProperties=" + Arrays.toString(otherProperties) +
+                '}';
+    }
 }
