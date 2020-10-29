4ce1e9032cccea107b445a2b20b5efae53da3b01
Ekaterina Marinova <katherineeyall@gmail.com>
2020-10-30 01:36:58 +0200

    populated the Constants.java

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -7,5 +7,7 @@ public final class Constants {
     public static final String PROPERTIES_PATH = ABSOLUTE_PATH + "/src/main/resources/application.properties";
     public static final String FILE_PATH_PROPERTY = "file_path";
     public static final String SHARED_LIST_NAME = "sharedVehicleList";
-
+    public static final String WHITE_SPACE = " ";
+    public static final String EMPTY_SPACE = "";
+    public static final String COMMA = ",";
 }
