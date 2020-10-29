1e1620d248faf76fa6efa02f80a4b5bcff94f96e
Ekaterina Marinova <katherineeyall@gmail.com>
2020-10-30 01:36:41 +0200

    created a function to add new cars from stdin

--- src/main/java/service/CatalogueServiceImpl.java
+++ src/main/java/service/CatalogueServiceImpl.java
@@ -1,24 +1,32 @@
 package service;
 
+import exception.InvalidVehicleTypeException;
 import record.Car;
+import record.CarType;
 import record.Vehicle;
 import service.definition.CatalogueService;
+import service.definition.MappingService;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
+
+import static util.Constants.*;
 
 public class CatalogueServiceImpl implements CatalogueService {
 
     private final List<Vehicle> vehicles;
+    private final MappingService mappingService;
 
-    public CatalogueServiceImpl(List<Vehicle> vehicles) {
+    public CatalogueServiceImpl(List<Vehicle> vehicles, MappingService mappingService) {
         this.vehicles = vehicles;
+        this.mappingService = mappingService;
     }
 
     @Override
     public void showCatalogue() {
-        for (Vehicle v:vehicles) {
+        for (Vehicle v : vehicles) {
             System.out.println(v.toString());
         }
     }
@@ -41,9 +49,45 @@ public class CatalogueServiceImpl implements CatalogueService {
         );
     }
 
+    @Override
+    public void addNewCar(Scanner scanner) throws InvalidVehicleTypeException {
+        System.out.println("Please input a car type - gas, electric or hybrid:");
+        var type = scanner.nextLine();
+
+        printMessageBasedOnType(type);
+
+        var properties = scanner.nextLine()
+                .toLowerCase()
+                .replace(WHITE_SPACE, EMPTY_SPACE)
+                .split(COMMA);
+        mappingService.mapObject(type, properties, vehicles);
+    }
+
     private List<Car> mapToCarList() {
         return vehicles.stream()
                 .map(v -> (Car) v)
                 .collect(Collectors.toList());
+    }
+
+    private void printMessageBasedOnType(String type) {
+        switch (type) {
+            case CarType.GAS:
+                System.out.println("""
+                        For gas car, enter the following properties on the next line:
+                        brand, model, power(KW), engine displacement(liters), price 
+                        """);
+                break;
+            case CarType.ELECTRIC:
+                System.out.println("""
+                        For electric car, enter the following properties on the next line:
+                        brand, model, power(KW), battery power(Ah), price
+                        """);
+                break;
+            case CarType.HYBRID:
+                System.out.println("""
+                        For hybrid car, enter the following properties on the next line:
+                        brand, model, displacement(l), power(KW), battery power(Ah), price
+                        """);
+        }
     }
 }
--- src/main/java/service/definition/CatalogueService.java
+++ src/main/java/service/definition/CatalogueService.java
@@ -1,7 +1,12 @@
 package service.definition;
 
+import exception.InvalidVehicleTypeException;
+
+import java.util.Scanner;
+
 public interface CatalogueService {
     void showCatalogue();
     void sortByCarType();
     void sortByBrand();
+    void addNewCar(Scanner scanner) throws InvalidVehicleTypeException;
 }
