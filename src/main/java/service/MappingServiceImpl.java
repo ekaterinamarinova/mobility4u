851e78b4d434f5c1f290918b4803e245f4460afa
Ekaterina Marinova <katherineeyall@gmail.com>
2020-10-30 01:38:35 +0200

    MappingService now correctly maps values

--- src/main/java/service/MappingServiceImpl.java
+++ src/main/java/service/MappingServiceImpl.java
@@ -1,34 +1,33 @@
 package service;
 
+import exception.InvalidVehicleTypeException;
 import record.Car;
 import record.CarType;
 import record.Vehicle;
 import service.definition.MappingService;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static util.Constants.*;
 
 public class MappingServiceImpl implements MappingService {
 
-    private static final String COMMA = ",";
-    private static final String WHITE_SPACE = " ";
-    private static final String EMPTY_STR = "";
     private static final String POSTFIX = "_car";
-
-    private List<Vehicle> vehicles;
+    private final List<Vehicle> vehicles;
 
     public MappingServiceImpl(List<Vehicle> vehicles) {
         this.vehicles = vehicles;
     }
 
-    public List<Vehicle> mapToObject(List<String> lines) {
+    public List<Vehicle> mapObjects(List<String> lines) throws InvalidVehicleTypeException {
         var fields = new String[]{};
 
         for (String s : lines) {
             var type = s.toLowerCase().substring(0, s.toLowerCase().indexOf(WHITE_SPACE));
 
             fields = s.toLowerCase()
-                    .replaceAll("\s++", EMPTY_STR)
+                    .replaceAll("\s++", EMPTY_SPACE)
                     .substring(
                             s.toLowerCase()
                                     .lastIndexOf(
@@ -36,24 +35,32 @@ public class MappingServiceImpl implements MappingService {
                                     ) + POSTFIX.length()
                     ).split(COMMA);
 
-            mapToSpecificObject(type, fields);
+            mapObject(type, fields, vehicles);
         }
 
         return vehicles;
     }
 
-    private void mapToSpecificObject(String type,
-                                     String[] fields) {
+    public void mapObject(String type,
+                          String[] fields,
+                          List<Vehicle> vehicles) throws InvalidVehicleTypeException {
         switch (type) {
+            /*
+            I would like to point out that I am initially against storing Integer values
+            in the Car record simply because of the over complication of calling replace
+            and valueOf functions, and when stored those values are later displayed as strings anyway.
+            But assuming we give up processor power over memory, this micro optimization would be valid.
+             */
+
             case CarType.GAS:
                 vehicles.add(
                         new Car(
                                 type,
                                 fields[0], //brand
                                 fields[1], //model
-                                fields[3], //power
-                                fields[4], //price
-                                fields[2]  //otherProps
+                                Integer.valueOf(fields[3].replaceAll("[a-z]", "")), //power
+                                Integer.valueOf(fields[4].replaceAll("[a-z]", "")), //price
+                                "engineDisplacement: " + fields[2]  //otherProps
                         )
                 );
                 break;
@@ -61,10 +68,11 @@ public class MappingServiceImpl implements MappingService {
                 vehicles.add(
                         new Car(
                                 type,
+                                fields[0],
                                 fields[1],
-                                fields[2],
-                                fields[4],
-                                null
+                                Integer.valueOf(fields[2].replaceAll("[a-z]", "")), //power
+                                Integer.valueOf(fields[4].replaceAll("[a-z]", "")),
+                                "batteryPower: " + fields[3]
                         )
                 );
                 break;
@@ -72,15 +80,20 @@ public class MappingServiceImpl implements MappingService {
                 vehicles.add(
                         new Car(
                                 type,
+                                fields[0],
                                 fields[1],
-                                fields[2],
-                                fields[3],
-                                fields[4]
+                                Integer.valueOf(fields[3].replaceAll("[a-z]", "")), //power
+                                Integer.valueOf(fields[5].replaceAll("[a-z]", "")),
+                                "engineDisplacement: " + fields[2],
+                                "batteryPower: " + fields[4]
                         )
                 );
                 break;
             default:
-                break;
+                throw new InvalidVehicleTypeException("The type received is invalid. Currently supported types are: " +
+                        Arrays.toString(CarType.values()) +
+                        " case insensitive.");
+
         }
     }
 
--- src/main/java/service/definition/MappingService.java
+++ src/main/java/service/definition/MappingService.java
@@ -1,9 +1,11 @@
 package service.definition;
 
+import exception.InvalidVehicleTypeException;
 import record.Vehicle;
 
 import java.util.List;
 
 public interface MappingService {
-    List<Vehicle> mapToObject(List<String> lines);
+    List<Vehicle> mapObjects(List<String> lines) throws InvalidVehicleTypeException;
+    void mapObject(String type, String[] fields, List<Vehicle> vehicles) throws InvalidVehicleTypeException;
 }
