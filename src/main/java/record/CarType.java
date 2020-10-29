97a323301df4c4cfdc82bc6a8c89a5cbefdee71c
Ekaterina Marinova <katherineeyall@gmail.com>
2020-10-30 01:35:59 +0200

    added a values() function that returns all currently set values

--- src/main/java/record/CarType.java
+++ src/main/java/record/CarType.java
@@ -1,7 +1,26 @@
 package record;
 
+import java.util.Arrays;
+
 public final class CarType {
     public static final String HYBRID = "hybrid_car";
     public static final String GAS = "gas_car";
     public static final String ELECTRIC = "electric_car";
+
+    /**
+     * Using Reflection, obtain currently declared fields' values.
+     * @return - an object array with the declared values
+     */
+    public static Object[] values() {
+        return Arrays.stream(CarType.class.getFields())
+                .map(field -> {
+                    try {
+                        return field.get(null);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                    return field.toString();
+                }
+        ).toArray();
+    }
 }
