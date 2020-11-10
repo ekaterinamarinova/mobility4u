b4e4a79ef6b372d42dd6fbe824d2e6a15cd6144d
ekaterina <e.marinova@space-cube.de>
2020-11-11 00:24:56 +0200

    renamed test

--- src/test/java/service/FileOpsServiceImplTest.java
+++ src/test/java/service/FileOpsServiceImplTest.java
@@ -5,7 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import service.definition.ReaderService;
+import service.definition.FileOpsService;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,17 +13,18 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @RunWith(JUnit4.class)
-public class ReaderServiceImplTest {
+public class FileOpsServiceImplTest
+{
 
     private static final String MOBILITY_TXT_PATH = "/src/test/resources/mobility.txt";
     private static final String EMPTY_STR = "";
 
-    private ReaderService readerService;
+    private FileOpsService fileOpsService;
     private Path pathToFile;
 
     @Before
     public void setUp() {
-        readerService = new ReaderServiceImpl();
+        fileOpsService = new FileOpsServiceImpl();
         var absoluteStringToFile = Paths.get(EMPTY_STR).toAbsolutePath().toString() + MOBILITY_TXT_PATH;
         pathToFile = Path.of(absoluteStringToFile);
     }
@@ -31,7 +32,7 @@ public class ReaderServiceImplTest {
     @Test
     public void testReadFile() throws IOException {
         var lineCount = Files.readAllLines(pathToFile).stream().count();
-        var result = readerService.readFile(pathToFile);
+        var result = fileOpsService.readFile(pathToFile);
 
         Assert.assertNotNull(result);
         Assert.assertEquals(lineCount, result.size());
@@ -39,7 +40,7 @@ public class ReaderServiceImplTest {
 
     @Test(expected = IOException.class)
     public void testReadFile_withInvalidPath() throws IOException {
-        readerService.readFile(Path.of(""));
+        fileOpsService.readFile(Path.of(""));
     }
 
 }
