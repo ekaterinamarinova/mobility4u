1a55187c8eb75bd5b33a6265c1976a1ecc5e4744
ekaterinamarinova <katherineeyall@gmail.com>
2020-11-20 23:31:01 +0200

    constants cleanup

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -6,10 +6,16 @@ public final class Constants {
     /**
      * paths
      */
+    private static final String MAIN_RESOURCES = "/src/main/resources";
+    private static final String TEST_RESOURCES = "/src/test/resources";
+
     public static final String ABSOLUTE_PATH = Path.of("").toAbsolutePath().toString();
-    public static final String PROPERTIES_PATH = ABSOLUTE_PATH + "/src/main/resources/application.properties";
-    public static final String DEFAULT_PATH_TO_CATALOG = ABSOLUTE_PATH + "/src/main/resources/";
-    public static final String PATH_TO_TEST_SIMULATED_INPUT_FILE = ABSOLUTE_PATH + "/src/test/resources/SimulatedUserInput.txt";
+
+    public static final String DEFAULT_PATH_TO_CATALOG = ABSOLUTE_PATH + MAIN_RESOURCES;
+    public static final String PROPERTIES_PATH = DEFAULT_PATH_TO_CATALOG + "/application.properties";
+
+    public static final String PATH_TO_TEST_SIMULATED_INPUT_FILE = ABSOLUTE_PATH + TEST_RESOURCES + "/example_user_input.txt";
+    public static final String PATH_TO_TEST_CATALOG = ABSOLUTE_PATH + TEST_RESOURCES + "/mobility.txt";
     public static final String FILE_PATH_PROPERTY = "file_path";
 
     /**
@@ -25,10 +31,9 @@ public final class Constants {
     public static final String WHITE_SPACE = " ";
     public static final String EMPTY_SPACE = "";
     public static final String COMMA = ",";
-    public static final String REGEX = "[A-Za-z,.;-]";
 
     /**
      * end of user stdin property
      */
-    public static final String STOP_WRITING_TO_FILE_WORD = "!close!";
+    public static final String STOP_WRITING_TO_FILE_WORD = "!stop!";
 }
